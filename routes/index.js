7ca0acc9f28fd556a5985e360fb4651f90f7717a
cali-rocket <eastonjubinpark@gmail.com>
2023-06-27 22:03:26 +0900

    mypage

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const postsRouter = require("./postsRouter");
+const mypageRouter = require("./mypageRouter");
 const commentsRouter = require("./commentsRouter");
 const authRouter = require("./authRouter");
 
 const router = express();
 
-router.use("/api", [postsRouter, commentsRouter, authRouter]);
+router.use("/api", [postsRouter, mypageRouter, commentsRouter, authRouter]);
 
 module.exports = router;
---
+++ routes/mypageRouter.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const router = express.Router();
+const auth = require("../middlewares/auth");
+const { Posts, Users, Categories, sequelize } = require("../models");
+const { Op } = require("sequelize");
+
+router.get("/mypage", auth, async (req, res) => {
+    try {
+        const user_id = res.locals.id
+        console.log(user_id.user_id)
+        // 게시글 목록 조회
+        const mypage = await Posts.findAll({
+            attributes: [
+                "post_id",
+                "user_id",
+                [sequelize.col("nickname"), "nickname"],
+                "title",
+                "content",
+                "image",
+                "category",
+                "scrap_num",
+                "like_num",
+                "created_at",
+                "updated_at",
+            ],
+            where: {user_id: user_id.user_id},
+            include: [
+                {
+                    model: Users,
+                    attributes: [],
+                },
+            ],
+            order: [["created_at", "DESC"]],
+            raw: true,
+        });
+
+        // 작성된 게시글이 없을 경우
+        if (mypage.length === 0) {
+            return res
+                .status(400)
+                .json({ message: "작성된 게시글이 없습니다." });
+        }
+        // 게시글 목록 조회
+        return res.status(200).json({ mypage });
+    } catch (e) {
+        // 예외 처리
+        console.log(e);
+        return res.status(400).json({ message: "목록 조회에 실패했습니다." });
+    }
+});
+module.exports = router;
