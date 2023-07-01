1ed305cc00b77f300138a439d280997475a9cfb2
cali-rocket <eastonjubinpark@gmail.com>
2023-07-01 13:50:41 +0900

    수정

--- app.js
+++ app.js
@@ -17,6 +17,9 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
+app.use(express.json({ limit : "50mb" }));
+app.use(express.urlencoded({ limit:"50mb", extended: false }));
+
 app.use("/", routes);
 
 app.get("/", async (req, res) => {
