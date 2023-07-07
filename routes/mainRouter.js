310c15ed57001eb1cbd8d11c59f7850c93300f27
cali-rocket <eastonjubinpark@gmail.com>
2023-07-07 13:59:17 +0900

    multer

--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -191,65 +191,65 @@ router.post("/search/:keyword/:page", async (req, res, next) => {
 });
 
 //추천 게시글 조회
-// router.get("/recommend", auth, async (req, res) => {
-//     try {
-//         const user_id = res.locals.id;
-//         const nickname = res.locals.user;
-//         const user_type = res.locals.type;
+router.get("/recommend", auth, async (req, res) => {
+    try {
+        const user_id = res.locals.id;
+        const nickname = res.locals.user;
+        const user_type = res.locals.type;
 
-//         // 게시글 목록 조회
-//         const recommend = await Posts.findAll({
-//             attributes: [
-//                 "post_id",
-//                 "user_id",
-//                 [sequelize.col("nickname"), "nickname"],
-//                 "title",
-//                 "content",
-//                 "image",
-//                 "category",
-//                 "scrap_num",
-//                 "like_num",
-//                 "created_at",
-//                 "updated_at",
-//             ],
-//             where: {
-//                 [Op.and]: [
-//                     {
-//                         created_at: {
-//                             [Op.lt]: d,
-//                         },
-//                     },
-//                     {
-//                         created_at: {
-//                             [Op.gte]: new Date(year, month, day - 6),
-//                         },
-//                     },
-//                 ],
-//             },
-//             include: [
-//                 {
-//                     model: Users,
-//                     attributes: [],
-//                 },
-//             ],
-//             group: ["Posts.post_id"],
-//             order: [["like_num", "DESC"]],
-//             raw: true,
-//         });
+        // 게시글 목록 조회
+        const recommend = await Posts.findAll({
+            attributes: [
+                "post_id",
+                "user_id",
+                [sequelize.col("nickname"), "nickname"],
+                "title",
+                "content",
+                "image",
+                "category",
+                "scrap_num",
+                "like_num",
+                "created_at",
+                "updated_at",
+            ],
+            where: {
+                [Op.and]: [
+                    {
+                        created_at: {
+                            [Op.lt]: d,
+                        },
+                    },
+                    {
+                        created_at: {
+                            [Op.gte]: new Date(year, month, day - 6),
+                        },
+                    },
+                ],
+            },
+            include: [
+                {
+                    model: Users,
+                    attributes: [],
+                },
+            ],
+            group: ["Posts.post_id"],
+            order: [["like_num", "DESC"]],
+            raw: true,
+        });
 
-//         // 작성된 게시글이 없을 경우
-//         if (topfive.length === 0) {
-//             return res
-//                 .status(400)
-//                 .json({ message: "작성된 게시글이 없습니다." });
-//         }
-//         // 게시글 목록 조회
-//         return res.status(200).json({ topfive });
-//     } catch (e) {
-//         // 예외 처리
-//         console.log(e);
-//         return res.status(400).json({ message: "목록 조회에 실패했습니다." });
-//     }
-// });
+        // 작성된 게시글이 없을 경우
+        if (topfive.length === 0) {
+            return res
+                .status(400)
+                .json({ message: "작성된 게시글이 없습니다." });
+        }
+        // 게시글 목록 조회
+        return res.status(200).json({ topfive });
+    } catch (e) {
+        // 예외 처리
+        console.log(e);
+        return res.status(400).json({ message: "목록 조회에 실패했습니다." });
+    }
+});
 
 module.exports = router;
