93fb697dc8614fa8ed7284adc7c2322f3aa5d2c0
ryu820 <rhju820@naver.com>
2023-07-23 23:54:41 +0900

    comment repository testcode start

--- test/unit/repositories/commentRepository.test.js
+++ test/unit/repositories/commentRepository.test.js
@@ -1,5 +1,14 @@
 const CommentRepository = require("../../../repositories/commentRepository")
-const { MycommentResultSchema, userIdInsertSchema, ChosenCommentResultSchema, MyChosenCommentResultSchema } = require("../../fixtures/commentFixture")
+const {
+    MycommentResultSchema,
+    userIdInsertSchema,
+    ChosenCommentResultSchema,
+    MyChosenCommentResultSchema,
+    postCommentInsertSchema,
+    checkCommentResultSchema,
+    checkCommentInsertSchema,
+    updateCommentInsertSchema
+} = require("../../fixtures/commentFixture")
 const { Users, Comments, sequelize, Posts } = require("../../../models");
 const sinon = require('sinon')
 
@@ -8,13 +17,13 @@ describe("내가쓴댓글 가져오기 test", () => {
     let commentFindAllStub;
     beforeEach(() => {
         jest.resetAllMocks();
-        commentFindAllStub = sinon.stub(Comments,"findAll")
+        commentFindAllStub = sinon.stub(Comments, "findAll")
     })
     afterEach(() => { // 스텁을 복원하여 원래의 동작으로 복구합니다.
         commentFindAllStub.restore();
     });
 
-    test("findAllCommentByUserId test", async() => {
+    test("findAllCommentByUserId test", async () => {
         //findAllCommentByUserId이 어떤 값을 반환해줄지 넣어주기
         commentFindAllStub.resolves(MycommentResultSchema)
         const comment = await commentRepository.findAllCommentByUserId(
@@ -66,13 +75,13 @@ describe("내가 채택한 댓글 가져오기 test", () => {
     let ChosenCommentFindAllStub;
     beforeEach(() => {
         jest.resetAllMocks();
-        ChosenCommentFindAllStub = sinon.stub(Comments,"findAll")
+        ChosenCommentFindAllStub = sinon.stub(Comments, "findAll")
     })
     afterEach(() => { // 스텁을 복원하여 원래의 동작으로 복구합니다.
         ChosenCommentFindAllStub.restore();
     });
 
-    test("getChosenComment test", async() => {
+    test("getChosenComment test", async () => {
         //getChosenComment이 어떤 값을 반환해줄지 넣어주기
         ChosenCommentFindAllStub.resolves(ChosenCommentResultSchema)
         const comment = await commentRepository.getChosenComment(
@@ -127,13 +136,13 @@ describe("나의 채택된 댓글 가져오기 test", () => {
     let MyChosenCommentFindAllStub;
     beforeEach(() => {
         jest.resetAllMocks();
-        MyChosenCommentFindAllStub = sinon.stub(Comments,"findAll")
+        MyChosenCommentFindAllStub = sinon.stub(Comments, "findAll")
     })
     afterEach(() => { // 스텁을 복원하여 원래의 동작으로 복구합니다.
         MyChosenCommentFindAllStub.restore();
     });
 
-    test("getMyChosenComment test", async() => {
+    test("getMyChosenComment test", async () => {
         //getMyChosenComment이 어떤 값을 반환해줄지 넣어주기
         MyChosenCommentFindAllStub.resolves(MyChosenCommentResultSchema)
         const comment = await commentRepository.getMyChosenComment(
@@ -153,7 +162,7 @@ describe("나의 채택된 댓글 가져오기 test", () => {
                 "created_at",
                 "updated_at",
             ],
-            where: { chosen: 1, user_id:userIdInsertSchema.user_id },
+            where: { chosen: 1, user_id: userIdInsertSchema.user_id },
             include: [
                 {
                     model: Posts,
@@ -167,3 +176,79 @@ describe("나의 채택된 댓글 가져오기 test", () => {
         expect(comment).toEqual(MyChosenCommentResultSchema);
     })
 })
+
+describe("댓글 작성하기 test", () => {
+    let commentRepository = new CommentRepository();
+    let PostCommentFindAllStub;
+    beforeEach(() => {
+        jest.resetAllMocks();
+        PostCommentFindAllStub = sinon.stub(Comments, "create")
+    })
+    afterEach(() => { // 스텁을 복원하여 원래의 동작으로 복구합니다.
+        PostCommentFindAllStub.restore();
+    });
+
+    test("postComment test", async () => {
+        //postComment이 어떤 값을 반환해줄지 넣어주기
+        PostCommentFindAllStub.resolves()
+        await commentRepository.postComment(
+            postCommentInsertSchema
+        )
+
+        expect(PostCommentFindAllStub.calledOnce).toBe(true);
+        expect(PostCommentFindAllStub.calledWith(postCommentInsertSchema))
+    })
+})
+
+describe("댓글 확인하기 test", () => {
+    let commentRepository = new CommentRepository();
+    let checkCommentFindAllStub;
+    beforeEach(() => {
+        jest.resetAllMocks();
+        checkCommentFindAllStub = sinon.stub(Comments, "findOne")
+    })
+    afterEach(() => { // 스텁을 복원하여 원래의 동작으로 복구합니다.
+        checkCommentFindAllStub.restore();
+    });
+
+    test("checkComment test", async () => {
+        //checkComment이 어떤 값을 반환해줄지 넣어주기
+        checkCommentFindAllStub.resolves(checkCommentResultSchema)
+        await commentRepository.checkComment({
+            comment_id: checkCommentInsertSchema.comment_id
+        })
+
+        expect(checkCommentFindAllStub.calledOnce).toBe(true);
+        expect(checkCommentFindAllStub.calledWith({ where: { comment_id: checkCommentInsertSchema.comment_id } }))
+    })
+})
+
+describe("댓글 수정하기 test", () => {
+    let commentRepository = new CommentRepository();
+    let updateCommentFindAllStub;
+    beforeEach(() => {
+        jest.resetAllMocks();
+        updateCommentFindAllStub = sinon.stub(Comments, "update")
+    })
+    afterEach(() => { // 스텁을 복원하여 원래의 동작으로 복구합니다.
+        updateCommentFindAllStub.restore();
+    });
+
+    test("updateComment test", async () => {
+        //updateComment이 어떤 값을 반환해줄지 넣어주기
+        updateCommentFindAllStub.resolves()
+        await commentRepository.updateComment(updateCommentInsertSchema)
+
+        expect(updateCommentFindAllStub.calledOnce).toBe(true);
+        expect(updateCommentFindAllStub.calledWith(
+            {
+                comment: updateCommentInsertSchema.comment,
+                image: updateCommentInsertSchema.image,
+            },
+            {
+                where: { comment_id: updateCommentInsertSchema.comment_id },
+            }
+        ))
+    })
+})
+
